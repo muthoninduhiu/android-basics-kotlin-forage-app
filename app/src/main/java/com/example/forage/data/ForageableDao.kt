a63ebfe070c65157e62e5b114d7fa4b85feabe11
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-07 11:18:27 +0300

    get items by id

--- app/src/main/java/com/example/forage/data/ForageableDao.kt
+++ app/src/main/java/com/example/forage/data/ForageableDao.kt
@@ -26,15 +26,17 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface ForageableDao {
 
-    // TODO: implement a method to retrieve all Forageables from the database
+    // DONE: implement a method to retrieve all Forageables from the database
     @Query("SELECT * FROM forageable_database")
     fun getForageables(): Flow<List<Forageable>>
 
-    // TODO: implement a method to retrieve a Forageable from the database by id
-    
+    // DONE: implement a method to retrieve a Forageable from the database by id
+    @Query("SELECT id FROM forageable_database")
+    fun getForageable(id: Long): Flow<Forageable>
 
     // TODO: implement a method to insert a Forageable into the database
     //  (use OnConflictStrategy.REPLACE)
+
 
     // TODO: implement a method to update a Forageable that is already in the database
 
